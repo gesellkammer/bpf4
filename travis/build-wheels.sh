6e258dc71af57298a1f4a2b4f89d60bf95e938f6
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2019-07-02 00:06:30 +0200

    more fixes to travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -6,7 +6,7 @@ yum install -y atlas-devel
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
@@ -17,6 +17,6 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    "${PYBIN}/pip" install bpf4 --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/nosetests" bpf4)
 done
