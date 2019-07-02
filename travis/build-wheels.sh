2ac92b98eaed824d8347e197540ff1567434722d
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2019-07-02 02:38:34 +0200

    trying to limit the build to python >= 3.7

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,7 +5,8 @@ set -e -x
 yum install -y atlas-devel
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+# for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/cp3*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
