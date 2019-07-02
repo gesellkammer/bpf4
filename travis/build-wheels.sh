c842dc91b89a6f5e2e1948360a73b4c20b407eab
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2019-07-02 03:00:55 +0200

    again...

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -6,13 +6,13 @@ yum install -y atlas-devel
 
 # Compile wheels
 # for PYBIN in /opt/python/*/bin; do
-# for PYBIN in /opt/python/cp3*/bin; do
-#    "${PYBIN}/pip" install -r /io/requirements.txt
-#    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-#done
+ for PYBIN in /opt/python/cp36*/bin; do
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
 
-pip install -r /io/requirements.txt
-pip wheel /io/ -w wheelhouse/
+# pip install -r /io/requirements.txt
+# pip wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
@@ -20,8 +20,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-# for PYBIN in /opt/python/*/bin/; do
-#     "${PYBIN}/pip" install bpf4 --no-index -f /io/wheelhouse
-#     (cd "$HOME"; "${PYBIN}/nosetests" bpf4)
-# done
-pip install bpf4 --no-index -f /io/wheelhoise
+for PYBIN in /opt/python/cp36*/bin/; do
+     "${PYBIN}/pip" install bpf4 --no-index -f /io/wheelhouse
+     (cd "$HOME"; "${PYBIN}/nosetests" bpf4)
+done
