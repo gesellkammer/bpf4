edbd7d45889eac4c3a00afedab96c61e0ec20c55
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2019-07-02 02:03:58 +0200

    trying to fix travis again

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,8 +5,7 @@ set -e -x
 yum install -y atlas-devel
 
 # Compile wheels
-# for PYBIN in /opt/python/*/bin; do
-for PYBIN in /opt/python/python3*/bin; do
+for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
